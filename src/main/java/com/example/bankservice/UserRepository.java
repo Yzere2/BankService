05c5be72ec4b5853cb3e1f4c282eb83b781d44f6
Yzere2 <traczmaciej1@gmail.com>
2022-10-12 10:40:30 +0200

    Initial

---
+++ src/main/java/com/example/bankservice/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.bankservice;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.lang.reflect.Array;
+import java.util.List;
+
+@Repository
+public class UserRepository{
+
+    }
